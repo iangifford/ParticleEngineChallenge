220234bb3d3b185cc0204454bdb9bd2c77428e13
Jack <ian@thegiffordteam.org>
2021-01-20 21:47:16 -0500

    clean particle

--- src/scripts/particle.js
+++ src/scripts/particle.js
@@ -5,31 +5,31 @@ const defaultMass = 1;
 class physicsParticle {
 
     constructor(context, x, y) {
-        this.x = x;
-        this.y = y;
-        this.vx = 0;
-        this.vy = 0;
-        this.ax = 0;
-        this.ay = 0;
-        this.mass = defaultMass;
-        this.airResistance = airResistance;
-        this.context = context;
-        this.dead = false;
-        this.objectCollision = false;
+            this.x = x;
+            this.y = y;
+            this.vx = 0;
+            this.vy = 0;
+            this.ax = 0;
+            this.ay = 0;
+            this.mass = defaultMass;
+            this.airResistance = airResistance;
+            this.context = context;
+            this.dead = false;
+            this.objectCollision = false;
 
 
-        this.image = new Image();
-        this.image.src = "assets/defaultparticle.png";
+            this.image = new Image();
+            this.image.src = "assets/defaultparticle.png";
 
-    }
-
+        }
+        //draw function (called each tick)
     draw() {
-        this.context.drawImage(this.image, this.x, this.y);
-    }
-
+            this.context.drawImage(this.image, this.x, this.y);
+        }
+        //update the particle each tick (position, acc, vel, etc)
     tick() {
         if (!this.dead) {
-            this.ay = gravity // Currently the only source of vertical acc
+            this.ay = gravity // Currently the only source of vertical acc for this
             this.ax = windShown; //Only horizontal acc
             this.vx += this.ax;
             this.vx *= airResistance;
